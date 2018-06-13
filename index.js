f82b4acf1314f2eab78e392ca06e92ab19ed9e30
jordancjq <jordancjq@hotmail.com>
2018-06-13 13:38:25 +0800

    remove webhook

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Telegraf = require('telegraf')
 
-const BOT_TOKEN = process.env.BOT_TOKEN
-
-const app = new Telegraf(BOT_TOKEN)
+const app = new Telegraf(process.env.BOT_TOKEN)
 
 app.start((ctx) => ctx.reply("Welcome to FindMeFood!"))
 app.help((ctx) => ctx.reply("Commands available: /fmf"))
@@ -10,5 +8,10 @@ app.help((ctx) => ctx.reply("Commands available: /fmf"))
 app.command('fmf', (ctx) => ctx.reply('Randomizing...'))
 app.command('top', (ctx) => ctx.reply('Listing...'))
 app.command('nearby', (ctx) => ctx.reply('Finding...'))
+
+app.on('text', (ctx) => {
+    const userMessage = ctx.message.text
+    return ctx.reply(`${ctx.message.from.username}: ${userMessage}`)
+})
 
 app.startPolling()
